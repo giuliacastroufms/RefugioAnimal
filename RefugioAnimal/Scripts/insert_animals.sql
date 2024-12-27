497f24553001f2a6448384aa54fd2f01934d2efb
Diogo Ferreira <sw@melhorescolha.net>
2024-12-27 16:57:15 -0400

    sql scripts

---
+++ RefugioAnimal/Scripts/insert_animals.sql
@@ -0,0 +1,10 @@
+INSERT INTO Animals (Name, Description, Species, AdoptionStatus)
+VALUES 
+('Garfield', 'O famoso gato preguiçoso que adora lasanha.', 1, 0),
+('Tom', 'Gato do desenho Tom e Jerry, sempre em perseguição.', 1, 0),
+('Felix', 'Felix, o Gato, é um dos primeiros gatos animados, famoso por sua inteligência.', 1, 0),
+('Cheshire', 'Gato do livro e filme Alice no País das Maravilhas.', 1, 0);
+('Pluto', 'Cachorro do Mickey Mouse, leal e divertido.', 0, 0),
+('Scooby-Doo', 'O famoso cachorro detetive que adora mistérios e lanches.', 0, 0),
+('Snoopy', 'Cachorro beagle de Charlie Brown, sonha em ser aviador.', 0, 0),
+('Brian', 'Brian Griffin, o cachorro da série animada Family Guy, inteligente e sarcástico.', 0, 0);

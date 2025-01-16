ebdbca4aefe57082de3bb50f12998ccd60b0ecba
giuliacgoncalves <goncalvesgiuliaa@gmail.com>
2025-01-16 15:43:42 -0400

    Criado o básico da tela de Especies, alterado o menu no Layout, e inserido informações na Home Page. Só o básico, ainda estou entendendo como funciona.

---
+++ RefugioAnimal/Models/AnimalSpeciesViewModel.cs
@@ -0,0 +1,10 @@
+﻿using RefugioAnimal.Models.DTOs;
+
+namespace RefugioAnimal.Models
+{
+    public class AnimalSpeciesViewModel
+    {
+        public List<AnimalDto>? Cats { get; set; }
+        public List<AnimalDto>? Dogs { get; set; }
+    }
+}
